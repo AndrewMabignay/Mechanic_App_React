75ea48164cad4cdd70221b9046a50c1e3dc6ea61
Andrew Mabignay <johnandrew.mabignay@gmail.com>
2026-07-28 10:57:10 +0800

    fix: resolve public route issue

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,25 +1,25 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoute = () => {
-    const token = localStorage.getItem("token");
-    const role = localStorage.getItem("role");
+    const token = localStorage.getItem("bike_mechanic_token");
+    const role = localStorage.getItem("bike_mechanic_role");
 
     if (!token) {
-        return <Outlet />
+        return <Outlet />;
     }
 
     switch (role) {
-        case "Cyclist":
-        case "Cyclist/Mechanic":
+        case "cyclist":
+        case "cyclist_mechanic":
             return <Navigate to="/cyclist" replace />;
 
-        case "Mechanic":
+        case "mechanic":
             return <Navigate to="/mechanic" replace />;
 
-        case "Bike Shop Owner":
-            return <Navigate to="/shop" replace />;
+        case "bike_shop_owner":
+            return <Navigate to="/shop/dashboard" replace />;
 
-        case "Administrator":
+        case "admin":
             return <Navigate to="/admin" replace />;
 
         default:
