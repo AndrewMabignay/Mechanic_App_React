c419e17087b50e586fbc1036055f8bdae6501167
Andrew Mabignay <johnandrew.mabignay@gmail.com>
2026-09-24 17:06:37 +0800

    refactor: remove useFindMechanics hook

--- src/features/service_request/hooks/useFindMechanic.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { findMechanic } from "../api/serviceRequestApi";
-
-export const useFindMechanic = () => {
-    return useMutation({
-        mutationFn: findMechanic,
-    });
-};
